1dbddb746c6b4405e8927ff73bbbe5f8cbad2d84
ViniciusChagas95 <viniciuschagas95@hotmail.com>
2024-06-12 10:57:05 -0300

    Apagando a autentificação LDAP para teste local

--- app.py
+++ app.py
@@ -3,25 +3,13 @@ import ldap3
 import oracledb
 from ldap3.core.exceptions import LDAPBindError
 
-# Função para autenticar usuário no LDAP
-def authenticate_user_ldap(username, password):
-    server = ldap3.Server('ldap://your_ldap_server', port=389, use_ssl=False)
-    conn = ldap3.Connection(server, user=f"your_domain\\{username}", password=password)
-    try:
-        if conn.bind():
-            return True
-    except LDAPBindError:
-        return False
-    finally:
-        conn.unbind()
-    return False
 
 # Função para verificar usuário no banco de dados Oracle
 def authenticate_user_db(username, password):
     try:
         # Conecte-se ao banco de dados Oracle
         dsn = oracledb.makedsn('db_host', 1521, service_name='db_service')
-        conn = oracledb.connect(user='db_user', password='db_password', dsn=dsn)
+        conn = oracledb.connect(user='rm550167', password='051095', dsn=dsn)
         cursor = conn.cursor()
         
         # Execute a consulta para verificar as credenciais
